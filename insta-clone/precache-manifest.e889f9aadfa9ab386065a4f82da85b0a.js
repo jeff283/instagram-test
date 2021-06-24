76fded51635bd0fb38774b17ace5ce3a88859e66
Jeff <njorogejeff2019@gmail.com>
2021-06-24 16:02:22 +0300

    production build

---
+++ insta-clone/precache-manifest.e889f9aadfa9ab386065a4f82da85b0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c502e9bd4113d06fd3dd2133cb9e9a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2470f5f4425229160069",
+    "url": "/static/css/main.17bacf03.chunk.css"
+  },
+  {
+    "revision": "c986bd7da8ed5f0a8eb2",
+    "url": "/static/js/2.5d55a8ec.chunk.js"
+  },
+  {
+    "revision": "f13b8ee62297cdeb55e53ad5d3f0bd7f",
+    "url": "/static/js/2.5d55a8ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2470f5f4425229160069",
+    "url": "/static/js/main.8b362034.chunk.js"
+  },
+  {
+    "revision": "f35af6f7ccead2fe5dbb",
+    "url": "/static/js/runtime-main.4422d9b2.js"
+  }
+]);
